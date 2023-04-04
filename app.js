11e82d62d3dbfb45926400d339ee685efaad9ab8
MushuLeDragon <dragon.mushu@hotmail.com>
2023-04-04 11:03:17 +0200

    component lifestyle into rendered Clock class

--- app.js
+++ app.js
@@ -20,10 +20,42 @@ class Welcome extends React.Component {
   }
 }
 
-function Home () {
+class Clock extends React.Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = { date: new Date() }
+    this.timer = null
+  }
+
+  componentDidMount() {
+    this.timer = window.setInterval(this.tick.bind(this), 1000)
+  }
+
+  componentDidUpdate() { }
+
+  componentWillUnmount() {
+    window.clearInterval(this.timer)
+  }
+
+  tick() {
+    this.setState({ date: new Date() })
+  }
+
+  render() {
+    return <div>
+      Il est {this.state.date.toLocaleDateString()} {this.state.date.toLocaleTimeString()}
+    </div>
+
+  }
+}
+
+function Home() {
   return <div>
     <Welcome name="John">Bonjour la maison de Jean</Welcome>
     <Welcome name="Bob" />
+    <Clock />
   </div>
 }
 
