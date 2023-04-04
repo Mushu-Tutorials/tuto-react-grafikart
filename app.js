00e09f900595d69847bb8dbb9e6d37c025cfc5ae
MushuLeDragon <dragon.mushu@hotmail.com>
2023-04-04 10:28:37 +0200

    episode 2: discover JSX

--- app.js
+++ app.js
@@ -4,9 +4,21 @@ function numberFormat(n) {
   return n.toString().padStart(2, '0')
 }
 function render() {
-  const title = <h1>
-    Bonjour tout le monde <span>{n % 2 ? numberFormat(n) : null}</span>
-  </h1>
+  const items = [
+    'Tache 1',
+    'Tache 2',
+    'Tache 3',
+  ]
+  const lis = items.map((item, k) => <li key={k}>{item}</li>)
+  const title = <React.Fragment>
+    <h1 className="title" id="title">
+      Bonjour tout le monde <span>{n}</span>
+    </h1>
+    <ul>
+      {lis}
+    </ul>
+  </React.Fragment>
+
   ReactDOM.render(title, document.querySelector('#app'))
 }
 
