bddaec63b9b8a4d3c6d1f0b57cd17b63198ff224
MushuLeDragon <dragon.mushu@hotmail.com>
2023-04-14 16:59:37 +0200

    create forms

--- README.md
+++ README.md
@@ -5,3 +5,9 @@
 # Requirements
 
 - Babel: Plugin to exec JSX [official website](https://babeljs.io/docs/babel-plugin-transform-react-jsx) and free live code converter JSX to JS [here](https://babeljs.io/repl#?browsers=defaults%2C%20not%20ie%2011%2C%20not%20ie_mob%2011&build=&builtIns=false&corejs=3.21&spec=false&loose=false&code_lz=Q&debug=false&forceAllTransforms=false&modules=false&shippedProposals=false&circleciRepo=&evaluate=false&fileSize=false&timeTravel=false&sourceType=module&lineWrap=true&presets=env%2Creact%2Cstage-2&prettier=false&targets=&version=7.21.4&externalPlugins=&assumptions=%7B%7D)
+
+Test Open commit
+
+# Run project
+
+ In vscode, run the terminal then run **Live Server** extention
--- app.js
+++ app.js
@@ -1,146 +1,40 @@
-function WelcomeFunc({ name, children }) {
-  return <div>
-    <h1>Bonjour {name}</h1>
-    <p>
-      {children}
-    </p>
-
-  </div>
-}
-
-class Welcome extends React.Component {
-  render() {
-    return <div>
-      <h1>Bonjour {this.props.name}</h1>
-      <p>
-        {this.props.children}
-      </p>
-    </div>
-  }
-}
-
-class Clock extends React.Component {
+class Home extends React.Component {
   constructor(props) {
-    super(props)
-
-    this.state = { date: new Date() }
-    this.timer = null
+    super(props);
+    this.state = {
+      name: 'Jean',
+      nametext: 'Jean a un texte',
+      selectname: 'demo2',
+      checked: true
+    }
+    this.handleChange = this.handleChange.bind(this)
   }
 
-  componentDidMount() {
-    this.timer = window.setInterval(this.tick.bind(this), 1000)
-  }
-  // componentDidUpdate() { }
-  componentWillUnmount() {
-    window.clearInterval(this.timer)
-  }
-  tick() {
-    this.setState({ date: new Date() })
-  }
-
-  render() {
-    return <div>
-      Il est {this.state.date.toLocaleDateString()} {this.state.date.toLocaleTimeString()}
-    </div>
-
-  }
-}
-
-class Incrementer extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = { n: props.start, m: props.step, timer: null }
-    this.toggle = this.toggle.bind(this)
-    this.reset = this.reset.bind(this)
-  }
-
-  componentDidMount() {
-    this.play()
-  }
-  // componentDidUpdate() { }
-  componentWillUnmount() {
-    window.clearInterval(this.state.timer)
-  }
-
-  increment() {
-    this.setState((state, props) => ({ n: state.n + state.m }))
-  }
-
-  pause() {
-    window.clearInterval(this.state.timer)
+  handleChange(event) {
     this.setState({
-      timer: null
+      name: event.target.value,
+      nametext: event.target.value,
+      selectname: event.target.value,
+      checked: event.target.checked
     })
   }
 
-  play() {
-    window.clearInterval(this.state.timer)
-    this.setState({
-      timer: this.timer = window.setInterval(this.increment.bind(this), 1000)
-    })
-  }
-
-  toggle() {
-    return this.state.timer ? this.pause() : this.play()
-  }
-
-  label() {
-    return this.state.timer ? 'Stop' : 'Play'
-  }
-
-  reset() {
-    this.pause()
-    this.play()
-    this.setState((state, props) => ({ n: props.start }))
-    // window.clearInterval(this.state.timer)
-    // return this.state.timer = this.reset
-  }
-
+  // state = {  }
   render() {
     return <div>
-      Value: {this.state.n}
-      <button onClick={this.toggle}>{this.label()}</button>
-      <button onClick={this.reset}>Reset</button>
+      {this.state.selectname}
+      <label htmlFor="nom">My name</label>
+      <input type="text" name="name" id="name" value={this.state.name} onChange={this.handleChange} />
+      <textarea name="nametext" id="nametext" value={this.state.nametext} onChange={this.handleChange}></textarea>
+      <select value={this.state.selectname} onChange={this.handleChange}>
+        <option value="demo1">Demo 1</option>
+        <option value="demo2">Demo 2</option>
+        <option value="demo3">Demo 3</option>
+      </select>
+      <input type="checkbox" checked={this.state.checked} onChange={this.handleChange} />
+      {this.state.checked ? <div>Checkbox cochée</div> : null}
     </div>
   }
-}
-
-Incrementer.defaultProps = {
-  start: 0,
-  step: 1
-}
-
-class ManuelIncrement extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = { n: 0, m: props.step }
-  }
-
-  manualIncrement(e) {
-    console.log(e)
-    e.preventDefault()
-
-    this.setState(function (state, props) {
-      return { n: state.n + 1 }
-    })
-  }
-
-  render() {
-    return <div>
-      Manual Increment: {this.state.n} <button onClick={this.manualIncrement.bind(this)}>Increment</button>
-    </div>
-  }
-}
-
-function Home() {
-  return <div>
-    <Welcome name="John">Bonjour la maison de Jean</Welcome>
-    <Welcome name="Bob" />
-    <Clock />
-    <ManuelIncrement />
-    <Incrementer />
-  </div>
 }
 
 ReactDOM.render(<Home />, document.querySelector('#app'))
