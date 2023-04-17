22fa3bc566b54863da7576dd04f16a939205eeab
MushuLeDragon <dragon.mushu@hotmail.com>
2023-04-17 11:11:46 +0200

    episode 6: create forms + fields components + control field states + submit datas

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
+// Create class
+class Field extends React.Component {
+  render() {
+    const { name, value, onChange, children } = this.props
+    return <div className="form-group">
+      <label htmlFor={name}>{children}</label>
+      <input type="text" value={value} onChange={onChange} id={name} name={name} className="form-control" />
+    </div>
+  }
+}
+
+// Create function
+function Checkbox({ name, value, onChange, children }) {
+  return <div className="form-check">
+    <input type="checkbox" checked={value} onChange={onChange} id={name} name={name} className="form-check-input" />
+    <label htmlFor={name} className="form-check-label">{children}</label>
+  </div>
+}
+
 class Home extends React.Component {
   constructor(props) {
     super(props);
@@ -7,6 +26,7 @@ class Home extends React.Component {
       newsletter: false
     }
     this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
   handleChange(event) {
@@ -18,24 +38,28 @@ class Home extends React.Component {
     })
   }
 
-  // state = {  }
+  handleSubmit(e) {
+    e.preventDefault()
+    const data = JSON.stringify(this.state)
+    this.setState({
+      name: '',
+      firstname: '',
+      newsletter: false
+    })
+  }
+
   render() {
     console.log('render')
-    return <div>
+    return <form className="container" onSubmit={this.handleSubmit}>
+      <Field name="name" value={this.state.name} onChange={this.handleChange}>Nom</Field>
+      <Field name="firstname" value={this.state.firstname} onChange={this.handleChange}>Prénom</Field>
+      <Checkbox name="newsletter" value={this.state.newsletter} onChange={this.handleChange}>S'inscrire a la newsletter</Checkbox>
+
       <div>
-        <label htmlFor="name">My name</label>
-        <input type="text" name="name" id="name" value={this.state.name} onChange={this.handleChange} />
-      </div>
-      <div>
-        <label htmlFor="firstname">My firstname</label>
-        <input type="text" name="firstname" id="firstname" value={this.state.firstname} onChange={this.handleChange} />
-      </div>
-      <div>
-        <label htmlFor="newsletter">Newsletter</label>
-        <input type="checkbox" name="newsletter" id="newsletter" checked={this.state.newsletter} onChange={this.handleChange} />
+        <button className="btn btn-primary">Envoyer</button>
       </div>
       {JSON.stringify(this.state)}
-    </div>
+    </form>
   }
 }
 
